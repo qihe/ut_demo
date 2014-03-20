ac429282d67b23cb014fdbcf6a4e294fb20fbb9a
thoughtmiracle <thoughtmiracle@gmail.com>
2014-03-20 19:13:34 +0800

    reset to un refactoring

--- 06-Testable/src/main/java/com/baidu/crm/Order.java
+++ 06-Testable/src/main/java/com/baidu/crm/Order.java
@@ -2,11 +2,7 @@ package com.baidu.crm;
 
 import java.util.List;
 
-/**
- * User: howard
- * Date: 11-11-30
- * Time: 上午11:21
- */
+
 public class Order {
     public List<OrderItem> getItems() {
         return items;
--- 06-Testable/src/main/java/com/baidu/crm/PriceService.java
+++ 06-Testable/src/main/java/com/baidu/crm/PriceService.java
@@ -1,10 +1,6 @@
 package com.baidu.crm;
 
-/**
- * User: howard
- * Date: 11-11-30
- * Time: 上午11:22
- */
+
 public class PriceService {
 
     public static double getProductPrice(OrderItem order) {
